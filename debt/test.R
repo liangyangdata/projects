005e44b74766f7721b5fc1411dd459dbd45a140d
fisheryounggod <1632164730@qq.com>
2024-10-27 15:12:05 +0800

    move data and raw to other reps

--- debt/test.R
+++ debt/test.R
@@ -1,5 +1,9 @@
-# library(tidyverse)
+library(tidyverse)
 library(tidymodels)
 tidymodels_prefer()
 data(ames)
-(ames <- ames |> mutate(Sale_Price = log10(Sale_Price)))
+ames <- ames |> mutate(Sale_Price = log10(Sale_Price))
+ames  %>% head()
+
+
+1+1
